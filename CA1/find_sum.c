7a00dc15a902e13c42a6d1b1adc175e46d14d687
manih1384 <manihosseini1384@gmail.com>
2025-10-08 09:05:06 +0330

    add find_sum

---
+++ CA1/find_sum.c
@@ -0,0 +1,104 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+#define OUTPUT "result.txt"
+
+static void reverse(char *s, int n) {
+  int i = 0, j = n - 1;
+  while (i < j) {
+    char t = s[i]; s[i] = s[j]; s[j] = t;
+    i++; j--;
+  }
+}
+
+static int int_to_str(int num, char *buf) {
+  int i = 0;
+
+
+// base case
+  if (num == 0) {
+    buf[i++] = '0';
+    buf[i] = '\0';
+    return i;
+  }
+
+
+// check if final result in neg
+  int neg = 0;
+  if (num < 0) {
+    neg = 1;
+    num = -num;   
+  }
+
+
+// extract digits (in reverse)
+  while (num > 0) {
+    buf[i++] = '0' + (num % 10);
+    num /= 10;
+  }
+
+//  add neg sign
+  if (neg) buf[i++] = '-';
+
+
+  reverse(buf, i);
+  //end of line
+  buf[i] = '\0';
+  return i;
+}
+
+
+
+
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf(2, "Usage: find_sum <string>\n");
+    exit();
+  }
+
+  char *s = argv[1];
+
+  int sum = 0;
+  int in_num = 0;
+  int cur = 0;
+
+  for (int i = 0; s[i]; i++) {
+    char c = s[i];
+    if (c >= '0' && c <= '9') {
+      in_num = 1;
+      cur = cur * 10 + (c - '0');
+    } else {
+      if (in_num) {
+        sum += cur;
+        cur = 0;
+        in_num = 0;
+      }
+    }
+  }
+  if (in_num) sum += cur; 
+
+
+
+  unlink(OUTPUT); // works like rm 
+
+
+  int fd = open(OUTPUT, O_CREATE | O_WRONLY); // create or open file
+
+  
+  if (fd < 0) {
+    printf(2, "find_sum: cannot create %s\n", OUTPUT);
+    exit();
+  }
+
+  char outbuf[32];
+  int len = int_to_str(sum, outbuf);
+  write(fd, outbuf, len); 
+  write(fd, "\n", 1);
+  close(fd);
+
+
+  exit();
+}
