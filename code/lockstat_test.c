dbea6c3247e0ad7a25a29bd6ab389c3b52d32c2d
hmdmhmd2004 <hamidmahmoodi1383@gmail.com>
2025-12-28 19:50:43 +0330

    update part-2, lockstat_test and proc.c, wait for further changes

--- code/lockstat_test.c
+++ code/lockstat_test.c
@@ -10,44 +10,59 @@ workload(int load)
   int i;
   for(i = 0; i < load; i++){
     uptime();
-    sleep(0.5);
+    getpid();
+    sleep(1);
   }
 }
 
 int
 main(void)
 {
-  uint64 scores[NCPU];
+  uint64 scores_initial[NCPU];
+  uint64 scores_final[NCPU];
   int i, pid;
   int numOfChildren = 8;
 
   printf(1, "Starting Lock Contention Test on ptable.lock...\n");
 
-  // Print Initial Stats
-  getlockstat(scores);
-  printf(1, "Initial Scores (Spins/Acquire):\n");
-  for(i=0; i<NCPU; i++) printf(1, "CPU %d: %d\n", i, (int)scores[i]);
+  getlockstat(scores_initial);
 
-  printf(1, "\nRunning workload with %d children...\n", numOfChildren);
-
-  // Create Contention (Fork children)
-  for(i = 0; i < numOfChildren; i++){
-    pid = fork();
-    if(pid == 0){
-      workload(10000);
-      exit();
+  printf(1, "\n[WORKLOAD REPORT] Initial\n");
+  for(i=0; i<NCPU; i++) {
+    if (scores_initial[i]>0) {
+      printf(1, "CPU %d: Workload Score = %d\n", i, scores_initial[i]);
     }
   }
 
-  // Wait for all to finish
+  printf(1, "\nRunning workload with %d children...\n", numOfChildren);
+
+  for(i = 0; i < numOfChildren; i++){
+    pid = fork();
+    if(pid == 0){
+      workload(1000);
+      exit();
+    }
+  }
   for(i = 0; i < numOfChildren; i++){
     wait();
   }
 
-  // Print Final Stats
-  getlockstat(scores);
-  printf(1, "\nFinal Scores (Spins/Acquire):\n");
-  for(i=0; i<NCPU; i++) printf(1, "CPU %d: %d\n", i, (int)scores[i]);
+  getlockstat(scores_final);
 
+  printf(1, "\n[WORKLOAD REPORT] Initial\n");
+  for(i=0; i<NCPU; i++) {
+    if (scores_initial[i]>0) {
+      printf(1, "CPU %d: Workload Score = %d\n", i, scores_initial[i]);
+    }
+  }
+
+  printf(1, "\n[WORKLOAD REPORT] (Final - Initial)\n");
+  for(i=0; i<NCPU; i++) {
+      if(scores_initial[i] > 0) {
+          int score_delta = (int)scores_final[i] - (int)scores_initial[i];
+          printf(1, "CPU %d: Workload Score = %d\n", i, score_delta);
+      }
+  }
+  
   exit();
 }
